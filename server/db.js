c650a020194de7ed3145d37316ec81a92ceecbf7
Daivasmara <daivasmara@gmail.com>
2020-05-18 22:35:29 +0700

    server refactoring

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
+
+const {
+  DB_USER: user,
+  DB_PASSWORD: password,
+  DB_HOST: host,
+  DB_PORT: port,
+  DB_DATABSE: database,
+} = process.env;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "andiganteng",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+  user,
+  password,
+  host,
+  port,
+  database,
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,81 +1,21 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// route imports
+const todos = require('./routes/todos');
+
 const app = express();
-const cors = require("cors");
-const pool = require('./db');
 
-//middleware
-app.use(cors());
-app.use(express.json());
-
-//routes
-
-//create a todo
-app.post("/todos", async (req, res) => {
-    try {
-        // console.log(req.body);
-
-        const {description} = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
-
-        res.json(newTodo.rows[0]);
-
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-//get all todo
-app.get("/todos", async(req, res) => {
-    try {
-        const allTodos = await pool.query("SELECT * FROM todo");
-
-        res.json(allTodos.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
-
-//get a todo
-app.get("/todos/:id", async(req,res) => {
-    try {
-        const {id} = req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
-
-        res.json(todo.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
-//update a todo
-app.put("/todos/:id" , async(req,res) => {
-    try {
-        const {id} = req.params;
-        const {description} = req.body;
-
-        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
-
-        res.json("Todo was updated!")
-    
-    } catch (err) {
-        console.error(err.message);
-    }
-})
-
-//delete a todo
-app.delete("/todos/:id", async(req, res) => {
-    try {
-        const {id} = req.params;
-        
-        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
-
-        res.json("Todo was deleted!");
-    } catch (err) {
-        console.log(err.message);
-    }
-})
+// middleware
+app.use(cors()); // Cross-Origin Resource Sharing
+app.use(express.json()); // bodyparser = untuk parsing dari request
+app.use(morgan('dev')); // server logging
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000");
+  console.log('server has started on port 5000');
 });
+
+// routes
+app.use('/todos', todos);
+// app.use('/users', users); // contoh route user
